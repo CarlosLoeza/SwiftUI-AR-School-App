78306c0fcad7121eb7b5e91322d5a47f88b21483
Carlos Loeza <cloeza1@mail.ccsf.edu>
2024-03-07 14:02:45 -0800

    Initial Commit

---
+++ SwiftUI-AR-School-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-AR-School-App
+//
+//  Created by Carlos on 3/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, earth!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-AR-School-App/SwiftUI_AR_School_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AR_School_AppApp.swift
+//  SwiftUI-AR-School-App
+//
+//  Created by Carlos on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AR_School_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
